57fdfc620ac6540c014d8c984dab5ab180566d3f
xiedali <notinmood@gmail.com>
2021-08-04 09:16:00 +0800

    添加功能验证:PHP之??和?:

--- PHP/SpecialSymbol/QuestionSymbol.php
+++ PHP/SpecialSymbol/QuestionSymbol.php
@@ -6,16 +6,29 @@
  * @company: 海澜&润拓
  */
 
-$myArray['a'] = 'AA';
-$myArray['b'] = "BB";
-$myArray['c'] = "";
+/**
+ * 总结:
+ * $b = $a ?? $c;
+ * 相当于 $b = isset($a) ? $a : $c;
+ * ————————————————————————————————
+ * $b = $a ?: $c; //就是缩略的原来的三元操作符 a?b:c
+ * 相当于 $b = !empty($a) ? $a : $c;
+ */
+
 $myArray['d'] = [];
 
-$target = $myArray['d'] ?: 'NNN';
-echo $target;
+$isSet = $myArray['d'] ?? '原来没有初始化,现新设置值为x';
+$noEmpty = $myArray['d'] ?: '原来值为empty,现在赋值为x';
+echo PHP_EOL . "是否已经设置值:$isSet";
+echo PHP_EOL . "是否非空:$noEmpty";
+//--output-————————————————
+//是否已经设置值:Array
+//是否非空:原来值为empty,现在赋值为x
 
-
-
-$pp = $myArray['d'] ?? 'NNN';
-echo "$pp";
-
+$isSet = $myArray['e'] ?? '原来没有初始化,现新设置值为x';
+$noEmpty = $myArray['e'] ?: '原来值为empty,现在赋值为x';
+echo PHP_EOL . "是否已经设置值:$isSet";
+echo PHP_EOL . "是否非空:$noEmpty";
+//--output-————————————————
+//是否已经设置值:原来没有初始化,现新设置值为x
+//是否非空:原来值为empty,现在赋值为x
