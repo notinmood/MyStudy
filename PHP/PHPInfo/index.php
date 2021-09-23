48b6d314a6f4d4f9b2bd1564b3e978ca317349ab
xiedali <notinmood@gmail.com>
2021-09-23 23:50:10 +0800

    添加设计模式研究

---
+++ PHP/PHPInfo/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @file   : index.php
+ * @time   : 19:28
+ * @date   : 2021/9/18
+ * @emailto: 9727005@qq.com
+ * @creator: ShanDong Xiedali
+ * @company: HiLand & RainyTop
+ */
+
+phpinfo();
