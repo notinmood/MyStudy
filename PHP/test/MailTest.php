87adfc89fcb638288f63e60f0cd909cb3fa802be
xiedali <notinmood@gmail.com>
2021-10-05 21:43:57 +0800

    feature: 添加OptionResolver的研究案例

---
+++ PHP/test/MailTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @file   : MailTest.php
+ * @time   : 12:44
+ * @date   : 2021/10/4
+ * @emailto: 9727005@qq.com
+ * @creator: ShanDong Xiedali
+ * @company: HiLand & RainyTop
+ */
+
+namespace PHP\Study\SymfonyStudy\OptionsResolver;
+
+
+use Codeception\Test\Unit;
+
+class MailTest extends Unit
+{
+
+}
