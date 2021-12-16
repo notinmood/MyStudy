077fb0298c027ad655b761f163b077f7761cc10a
解大劦 <9727005@qq.com>
2021-12-16 10:31:37 +0800

    feature: 添加页面内部滚动位置的定位使用案例

---
+++ Python/函数重载/实现/index.py
@@ -0,0 +1,17 @@
+"""
+ * @file   : index.py
+ * @time   : 8:33
+ * @date   : 2021/12/14
+ * @mail   : 9727005@qq.com
+ * @creator: ShanDong Xiedali
+ * @company: HiLand & RainyTop
+"""
+import math
+
+
+def area(radius):
+    return math.pi * radius ** 2
+
+
+# print(locals())
+# print(globals())
