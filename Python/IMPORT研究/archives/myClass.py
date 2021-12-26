3382cb38eff903795bc64296d716d7d749524c69
解大劦 <9727005@qq.com>
2021-12-26 19:01:59 +0800

    feature: 添加__import__动态调用模块和功能的研究

---
+++ Python/IMPORT研究/archives/myClass.py
@@ -0,0 +1,18 @@
+print('in myClass.py')
+
+
+def __index__():
+    print('in myClass index')
+
+
+def say_hello(self):
+    print('in myClass.py say hello')
+
+
+class MyClass(object):
+    """
+
+    """
+
+    def say_hello(self):
+        print('in myClass say hello')
--- Python/IMPORT研究/archives/role.py
+++
@@ -1,9 +0,0 @@
-# role.py
-from hilandBasicLibrary.data.reflectHelper import ReflectHelper
-
-print('role')
-
-
-def say_hello():
-    print('hello role')
-    print(ReflectHelper.get_current_method_name())
