1db58c8b36ca829e1a7d3dd6356f2576627becd0
解大劦 <9727005@qq.com>
2022-01-31 06:40:15 +0800

    feature: 添加JavaScript中call、apply、bind的使用研究

--- WebStudy/Call_Apply_Bind/using_GetType.js
+++ WebStudy/Call_Apply_Bind/using_GetType.js
@@ -7,6 +7,13 @@
  * @company: HiLand & RainyTop
  */
 
+// +--------------------------------------------------------------------------
+// |::说明：| 这是一个调用toString方法的例子：
+// |·······|  -在一个对象上直接调用toString方法，那么是调用这个对象上被重写过的toString方法
+// |·······|  -如果通过call调用Object对象上的toString方法，是调用JS系统最原始的toString方法
+// |·······|  -(JavaScript系统最原始的toString方法,返回的其格式为字符串[object Xxx],其中 Xxx 就是对象的真实类型。)
+// +--------------------------------------------------------------------------
+
 let target = 1;
 let result = target.toString();
 console.log(result);
