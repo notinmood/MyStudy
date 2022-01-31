a083bb237f3ac588610776b3d751800008822e83
解大劦 <9727005@qq.com>
2022-01-31 09:08:31 +0800

    feature: 添加运算符instanceof的研究案例

---
+++ PHP/ArrayStudy/1.array_shift.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @file   : 1.array_shift.php
+ * @time   : 7:03
+ * @date   : 2022/1/31
+ * @mail   : 9727005@qq.com
+ * @creator: ShanDong Xiedali
+ * @company: HiLand & RainyTop
+ */
+
+$a=array("a"=>"red","b"=>"green","c"=>"blue");
+echo array_shift($a);
+echo PHP_EOL;
+echo '────────────────────────';
+echo PHP_EOL;
+print_r ($a);
