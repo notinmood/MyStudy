13a3c448ec4ed88db14fc1e4f05f5f97ce412051
解大劦 <9727005@qq.com>
2022-02-18 08:50:52 +0800

    docs: 添加说明文档

--- WebStudy/NamespaceStudy/index.ts
+++ WebStudy/NamespaceStudy/index.ts
@@ -1,8 +1,11 @@
-import {YourNameSpace,hah,MyNameSpace as aa} from './thirdClass'
+/**
+ * 如果待引入的项目跟本地其他项目重名，就可以给待引入的项目指定别名
+ */
+import {hah, MyNameSpace as aa, YourNameSpace} from './thirdClass'
 
-namespace MyNameSpace{
-    export let myName:string='zhangsan';
-    export let mySchool:string ="QDU";
+namespace MyNameSpace {
+    export let myName: string = 'zhangsan';
+    export let mySchool: string = "QDU";
 }
 
 console.log(aa.getName());
