f8f356bf9966c82eeded99a4806c3a8253b15b94
解大劦 <9727005@qq.com>
2022-02-27 22:15:59 +0800

    feature: 添加JavaScript中目标数据为引用类型还是值类型的判断研究

---
+++ WebStudy/JavaScript的类型判断/index.js
@@ -0,0 +1,47 @@
+/**
+ * @file   : index.js
+ * @time   : 21:16
+ * @date   : 2022/2/27
+ * @mail   : 9727005@qq.com
+ * @creator: ShanDong Xiedali
+ * @company: HiLand & RainyTop
+ */
+
+function isReferenceType(value) {
+    if (value === null) {
+        return true;
+    }
+    return value instanceof Object;
+}
+
+console.log(isReferenceType(123));
+
+console.log(isReferenceType(true));
+
+console.log(isReferenceType("123"));
+
+console.log(isReferenceType(undefined));
+
+
+console.log(isReferenceType(function doSomething() {
+}));
+
+console.log(isReferenceType(new Date()));
+
+console.log(isReferenceType({}));
+
+console.log(isReferenceType([]));
+
+console.log(isReferenceType(Number(123)));
+// noinspection all
+console.log(isReferenceType(new Number(123)));
+
+
+// @formatter:off
+let a =   123;
+// @formatter:on
+
+
+console.log(typeof null);
+console.log(isReferenceType(null));
+
