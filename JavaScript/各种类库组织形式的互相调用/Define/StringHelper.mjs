5bbb426d1278462c62970be064bbf67b7d21c049
解大劦 <9727005@qq.com>
2022-03-04 16:43:00 +0800

    feature: 添加ES6创建对象的方法研究

--- JavaScript/各种类库组织形式的互相调用/Define/StringHelper.mjs
+++ JavaScript/各种类库组织形式的互相调用/Define/StringHelper.mjs
@@ -12,14 +12,14 @@ String.prototype.format = function (args) {
     }
 
     let data = arguments;		//如果模板参数是数组
-    if (arguments.length == 1 && typeof (args) == "object") {
+    if (arguments.length === 1 && typeof (args) == "object") {
         //如果模板参数是对象
         data = args;
     }
 
     for (let key in data) {
         let value = data[key];
-        if (undefined != value) {
+        if (undefined !== value) {
             result = result.replace("{" + key + "}", value);
         }
     }
