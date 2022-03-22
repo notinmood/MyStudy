0b8d59a763161ce9e47f945c9cb708aabc932747
解大劦 <9727005@qq.com>
2022-03-22 23:02:18 +0800

    feature: 完成Proxy的使用案例

--- JavaScript/自定义属性研究/Proxy/02.拦截function.js
+++ JavaScript/自定义属性研究/Proxy/02.拦截function.js
@@ -7,14 +7,30 @@
  * @company: HiLand & RainyTop
  */
 
-function sub(a, b){
+// +--------------------------------------------------------------------------
+// |::说明·| Proxy除了可以拦截一般的object对象外，还能拦截 function，数组甚至其他Proxy
+// +--------------------------------------------------------------------------
+
+function sub(a, b) {
     return a - b;
 }
+
 let handler = {
-    apply: function(target, ctx, args){
-        console.log('handle apply');
-        return Reflect.apply(...arguments);
+    apply: function (target, ctx, args) {
+        console.log('handle apply。应用 handler 处理函数');
+
+        // // 方式1
+        // return Reflect.apply(...arguments);
+
+        // // 方式2
+        // console.log(target);
+        // console.log(args);
+        // return target(...args);
+
+        // 方式3
+        return Reflect.apply(target, ctx, args);
     }
 }
 let proxy = new Proxy(sub, handler);
-console.log(proxy(2, 1));
+let myValue = proxy(100, 20);
+console.log(myValue);
