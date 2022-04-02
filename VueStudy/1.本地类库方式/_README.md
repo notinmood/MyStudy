d0334f52b2f889011967e53c4da9c9b2ad33669c
解大劦 <9727005@qq.com>
2022-04-02 18:36:32 +0800

    切换IDE

--- VueStudy/1.本地类库方式/_README.md
+++ VueStudy/1.本地类库方式/_README.md
@@ -1,5 +1,6 @@
-说明
---
+# 说明
+> !!! cli模式的应用 使用 vsc +  Volar 扩展。暂时不使用 WebStorm。
+
 根据页面 `https://www.cnblogs.com/jjgw/p/15767238.html` 建立
 
 1. 目录 vue_test_ts 为 TypeScript 项目
---
+++ VueStudy/1.本地类库方式/vue_test_js/src/components/pp.vue
@@ -0,0 +1,12 @@
+// E:\myworkspace\MyStudy\VueStudy\1.本地类库方式\vue_test_js\src\components
+<script>
+ const chunk = (arr, size) =>
+   Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
+     arr.slice(i * size, i * size + size)
+   );
+
+
+</script>
+<template>
+    <div href=""> </div>
+</template>
