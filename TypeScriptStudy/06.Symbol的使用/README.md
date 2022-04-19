bdf9597d59ea90cb51ae5462239fc79ba3f6ea5a
解大劦 <9727005@qq.com>
2022-04-19 11:09:53 +0800

    feature: 添加对集合进行解构的学习

---
+++ TypeScriptStudy/06.Symbol的使用/README.md
@@ -0,0 +1,3 @@
+# 说明
+
+关于Symbol更多使用信息，参考JavaScript下 `E:\myworkspace\MyStudy\JavaScript\Symbol使用研究`
---
+++ TypeScriptStudy/07.泛型/index.ts
@@ -0,0 +1,13 @@
+/**
+ * @file   : aa.ts
+ * @time   : 10:23
+ * @date   : 2022/4/19
+ * @mail   : 9727005@qq.com
+ * @creator: ShanDong Xiedali
+ * @company: HiLand & RainyTop
+ */
+
+let myNumberArray: Array<Number> = [1, 2, 3, 5];
+let myNumberArray2: Number[] = [2, 4, 6];
+
+let myGenArray: Array<Number | string> = [1, "qingdao", 5];
