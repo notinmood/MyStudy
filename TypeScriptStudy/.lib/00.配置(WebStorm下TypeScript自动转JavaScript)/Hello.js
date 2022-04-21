071877374e644785942b9d6b1a01ebabbf5a21d3
解大劦 <9727005@qq.com>
2022-04-21 18:07:31 +0800

    删除lib目录

--- TypeScriptStudy/.lib/00.配置(WebStorm下TypeScript自动转JavaScript)/Hello.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-var aaa = "Hello world";
-var bbb = "I like this game！";
-console.log(aaa + bbb);
--- TypeScriptStudy/.lib/03.命名空间的使用/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-/// <reference path="../_res/Employee.ts">
-var e = new TSS._res.Employee("zhangsan", 20);
-var age = e.getAge();
-console.log(age);
