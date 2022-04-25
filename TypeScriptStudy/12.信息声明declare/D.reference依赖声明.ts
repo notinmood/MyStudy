cde8b69cd301ce5b6f41a8e74737e0d8cbc4a2ed
解大劦 <9727005@qq.com>
2022-04-25 16:22:49 +0800

    feature: 添加类型声明的研究

--- TypeScriptStudy/12.信息声明declare/D.reference依赖声明.ts
+++ TypeScriptStudy/12.信息声明declare/D.reference依赖声明.ts
@@ -1,7 +1,16 @@
+/**
+ * @file   : D.reference依赖声明.ts
+ * @time   : 9:11
+ * @date   : 2022/4/25
+ * @mail   : 9727005@qq.com
+ * @creator: ShanDong Xiedali
+ * @company: HiLand & RainyTop
+ */
+
 /// <reference path="../00.res/GloClass.ts" />
 
 
-
-//1. reference 语句只能出现在文件头部，其前面不能再有其他语句；否则就是一个普通的注释语句，无法起到声明依赖作用。
+// 说明
+//1. reference 语句只能出现在文件头部，其前面不能再有其他正式语句（但可以有注释语句）；否则就是一个普通的注释语句，无法起到声明依赖作用。
 //2. reference 语句什么依赖，不是导入文件。声明依赖的作用仅在 TypeScript编译器中进行代码提示。
 //3. 现代 IDE(VSC、WebStorm等)，不需要 reference 语句，依然可以识别出现目标类型，自动进行代码智能提示。
