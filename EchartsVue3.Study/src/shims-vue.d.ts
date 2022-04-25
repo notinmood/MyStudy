53c684f5477116fe071e715ef8dc06a69544bf77
解大劦 <9727005@qq.com>
2022-04-25 18:58:02 +0800

    feature: 添加详细的tsconfig.json配置项说明

--- EchartsVue3.Study/src/shims-vue.d.ts
+++ EchartsVue3.Study/src/shims-vue.d.ts
@@ -1,9 +1,10 @@
 // 1. 使用TypeScript的情形
-/* eslint-disable */
-declare module '*.vue' {
-    import type {DefineComponent} from 'vue'
-    const component: DefineComponent<{}, {}, any>
-    export default component
+declare module "*.vue" {
+    import type {DefineComponent} from "vue";
+
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    const component: DefineComponent<{}, {}, any>;
+    export default component;
 }
 
 // 2. 使用JavaScript的情形
