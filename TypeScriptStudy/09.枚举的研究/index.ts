84d534ade46d6350ccf02d8edd698499daac81e4
解大劦 <9727005@qq.com>
2022-05-06 16:23:34 +0800

    refactor: 完善TypeScript中enum的研究案例

--- TypeScriptStudy/09.枚举的研究/index.ts
+++ TypeScriptStudy/09.枚举的研究/index.ts
@@ -7,14 +7,54 @@
  * @company: HiLand & RainyTop
  */
 
-// +--------------------------------------------------------------------------
-// |::说明·| 特别注意：枚举后面的{}内不是对象的结构形式。
-// +--------------------------------------------------------------------------
-enum MyColor {
-    Red,
-    Green,
-    Yellow,
+export namespace MyNameSpace {
+    // +--------------------------------------------------------------------------
+    // |::说明·| 特别注意：枚举后面的{}内不是对象的结构形式。
+    // +--------------------------------------------------------------------------
+    enum MyColor {
+        Red,
+        Green,
+        Yellow,
+    }
+
+    console.log(MyColor.Yellow); // 2
+    console.log(MyColor['Yellow']);// 2
+    console.log(MyColor[2]);// Yellow
+
+
+
+    /**
+     * 以下是编译后形成的 JavaScript 文件
+     */
+
+    // var MyColor;
+    // (function (MyColor) {
+    //     MyColor[MyColor["Red"] = 0] = "Red";
+    //     MyColor[MyColor["Green"] = 1] = "Green";
+    //     MyColor[MyColor["Yellow"] = 2] = "Yellow";
+    // })(MyColor || (MyColor = {}));
+    // console.log(MyColor.Yellow); // 2
+    // console.log(MyColor[2]); // Yellow
+
+    /**
+     * 枚举量也可以是 字符串
+     */
+    enum Direction {
+        NORTH = "NORTH",
+        SOUTH = "SOUTH",
+        EAST  = "EAST",
+        WEST  = "WEST",
+    }
+
+    /**
+     * 以下是编译后形成的 JavaScript 文件
+     */
+    // var Direction;
+    // (function (Direction) {
+    //     Direction["NORTH"] = "NORTH";
+    //     Direction["SOUTH"] = "SOUTH";
+    //     Direction["EAST"] = "EAST";
+    //     Direction["WEST"] = "WEST";
+    // })(Direction || (Direction = {}));
 }
 
-console.log(MyColor.Yellow); // 2
-console.log(MyColor[2]);// Yellow
