2474ca5cccf7787e7f3477a93930f0e2bd415779
解大劦 <9727005@qq.com>
2022-05-10 11:47:32 +0800

    feature:协变逆变研究

--- DotNet/dotnetStudy/CoreConsoleApp/Program.cs
+++ DotNet/dotnetStudy/CoreConsoleApp/Program.cs
@@ -3,12 +3,19 @@ using CoreConsoleApp.协变和逆变研究;
 
 namespace CoreConsoleApp
 {
+    /// <summary>
+    /// 
+    /// </summary>
     class Program
     {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
             Client.index();
         }
     }
