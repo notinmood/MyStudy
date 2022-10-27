f1372a609ebc4853d9620c0016a57c7d979c063f
解大劦 <9727005@qq.com>
2022-10-27 09:59:27 +0800

    feature: 添加JavaScript代码同步执行和异步执行的研究

---
+++ TypeScript.Gulp.Study/src/main.ts
@@ -0,0 +1,13 @@
+/**
+ * @file   : main.ts
+ * @time   : 15:30
+ * @date   : 2022/8/23
+ * @mail   : 9727005@qq.com
+ * @creator: ShanDong Xiedali
+ * @company: HiLand & RainyTop
+ */
+
+function hello(compiler: string) {
+    console.log(`Hello from ${compiler}`);
+}
+hello("TypeScript");
