9e893f5f8ca2542cb572e186b8dd8009f2985ace
山东解大劦 <notinmood@gmail.com>
2024-01-01 08:10:00 +0800

    refactor: 重构图片图层的查找功能可以重现

--- Python/操作PhotoShop/使用第三方类库photoshop-python-api/1.访问PSD中图层属性的实践.py
+++ Python/操作PhotoShop/使用第三方类库photoshop-python-api/1.访问PSD中图层属性的实践.py
@@ -7,21 +7,32 @@
  * @company: HiLand & RainyTop
 """
 import photoshop.api as psapi
+from BasicLibrary.data.randomHelper import RandomHelper
+from BasicLibrary.enums import RandomEnum
+from BasicLibrary.io.fileHelper import FileHelper
 from BasicLibrary.io.pathHelper import PathHelper
 from _utils.psUtil import PsUtil
 
 if __name__ == '__main__':
     # 文件名称
-    file_base_name = '_464_rmrb.psd'
+    file_base_name_no_extension = 'layerPropertyStudy'
+    file_extension_name = '.psd'
+    file_base_name = file_base_name_no_extension + file_extension_name
     current_path = PathHelper.get_dir_name(__file__, 1)
-    file_full_name = PathHelper.combine(current_path, "_res", file_base_name)
-    print(file_full_name)
+    res_path = PathHelper.combine(current_path, "_res")
+    file_full_name = PathHelper.combine(res_path, file_base_name)
+
+    new_file_base_name = f'{file_base_name_no_extension}_{RandomHelper.create(5, random_type=RandomEnum.LowerLetters)}{file_extension_name}'
+    new_file_full_name = PathHelper.combine(res_path, new_file_base_name)
+    FileHelper.copy(file_full_name, res_path, new_file_base_name)
+    print(f'正在访问的文件为：{file_base_name}')
 
     # 打开PhotoShop应用程序
     app = psapi.Application()
+    print('app.version:', app.version)
 
     # 打开指定的图片文件
-    doc = app.open(file_full_name)
+    doc = app.open(new_file_full_name)
 
     # 获取所有图层
     layers = doc.layers
@@ -35,13 +46,15 @@ if __name__ == '__main__':
     print('layer.allLocked:', layer.allLocked)
 
     # 查找被嵌套的图层
-    _layer_name = '/2024/01/01/'
+    _layer_name = '/2028/10/11/'
     layer_nested = PsUtil.find_layer(layers, _layer_name)
     if layer_nested is None:
         print('未找到图层')
     else:
-        layer_nested.textItem.contents = '/2028/10/11/'
+        layer_nested.textItem.contents = '/2030/01/01/'
         print('图层名称修改成功')
     pass
 
-
+    #  关闭文档及其清理其他各种资源
+    doc.close()
+    FileHelper.remove(new_file_full_name)
