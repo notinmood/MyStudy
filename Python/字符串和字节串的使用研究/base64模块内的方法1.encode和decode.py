b8cfa750d8078f9c799dd0a14ae43f4b9c8f128b
山东解大劦 <notinmood@gmail.com>
2024-01-12 18:14:16 +0800

    refactor: 完成字符串和字节串子目录结构重构

---
+++ Python/字符串和字节串的使用研究/base64模块内的方法1.encode和decode.py
@@ -0,0 +1,14 @@
+"""
+ * @file   : base64模块内的方法1.encode和decode.py
+ * @time   : 11:09
+ * @date   : 2024/1/12
+ * @mail   : 9727005@qq.com
+ * @creator: ShanDong Xiedali
+ * @company: HiLand & RainyTop
+"""
+
+import base64
+
+if __name__ == '__main__':
+    i = 1
+pass
