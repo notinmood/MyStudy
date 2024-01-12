c675ceda03e002c74cff9a5cea9023e252f6ae95
山东解大劦 <notinmood@gmail.com>
2024-01-12 09:46:21 +0800

    refactor: 对泛型方法和泛型类型的定义使用案例，进行重构

--- Python/字符串和字节串的使用研究/6.字节串的研究.py
+++ Python/字符串和字节串的使用研究/6.字节串的研究.py
@@ -6,9 +6,12 @@
  * @creator: ShanDong Xiedali
  * @company: HiLand & RainyTop
 """
+
+# TODO:xiedali@2024/01/12 应该把Base64的研究内容从其他目录整合过来
+
 if __name__ == '__main__':
     # 1. 字节串的定义
-    my_bytes = b"abcdefg"
+    my_bytes = b"abcd"
     # 以下是为了消除ide参数的警告而做的PolyFill
     # noinspection all
     print(my_bytes)  # b'abcd'
@@ -16,7 +19,7 @@ if __name__ == '__main__':
     # 以下是为了消除ide的警告而做的PolyFill
     # noinspection all
     print(my_bytes[2:3])  # b'c'
-    print(my_bytes[-1])  # 103
+    print(my_bytes[-1])  # 100
 
     # # b包含的字符串称为字节串，但其内部仅支持ASCII字符，不能直接包含中文
     # # 以下代码会出现错误：“SyntaxError: bytes can only contain ASCII literal characters”
@@ -24,10 +27,18 @@ if __name__ == '__main__':
     # print(my_byte_string)
 
     # 2. 字节串和字节串的编码解码转换
-    my_bytes = b"\xe4\xb8\xad\xe6\x96\x87"
-    my_string = my_bytes.decode()
-    print(my_string)  # 中文
+    s = "中文"
+    b = s.encode()  # 默认使用utf-8
+    print(b)  # b'\xe4\xb8\xad\xe6\x96\x87'
 
     s = "中文"
-    b = s.encode()
-    print(b)  # b'\xe4\xb8\xad\xe6\x96\x87'
+    b = s.encode(encoding="gbk")  # 明确使用GBK编码规则
+    print(b)  # b'\xd6\xd0\xce\xc4'
+
+    my_bytes = b"\xe4\xb8\xad\xe6\x96\x87"
+    my_string = my_bytes.decode()  # 默认使用utf-8
+    print(my_string)  # 中文
+
+    my_bytes = b'\xd6\xd0\xce\xc4'
+    my_string = my_bytes.decode(encoding="gb2312")  # gb2312是GBK的一个子集，是可以对GBK编码的bytes进行解码的
+    print(my_string)  # 中文
--- Python/类型Study/B.定义和使用泛型类型.py
+++ Python/类型Study/B.定义和使用泛型类型.py
@@ -14,16 +14,16 @@ from typing import TypeVar, Generic
 # ---------------------------------------------------------------------------
 # 定义泛型类型的目的是控制对象实例中的某些属性的类型为泛型。定义泛型类型的时候：
 #   1. 需要从typing模块导入TypeVar，然后使用TypeVar来声明一个名称为T的泛型类型
-#   2. 类型需要从Generic[T]派生
+#   2. 泛型类型需要从Generic[T]派生；或者直接在方法名称后面通过方括号[U, V]指定泛型参数
 # +--------------------------------------------------------------------------
 
 T = TypeVar("T")
 # 如果类型参数有多个，可以类似格式继续定义
 U = TypeVar("U")
-S = TypeVar("S")
+V = TypeVar("V")
 
 
-# 1. 定义泛型类型
+# 1.1 定义泛型类型的方式1：从Generic[T]派生(带1个类型参数)
 @dataclass
 class ReturnResult(Generic[T]):
     """
@@ -34,10 +34,35 @@ class ReturnResult(Generic[T]):
     data: T = None
 
 
+# 1.2 定义泛型类型的方式1：从Generic[T]派生(带多个类型参数)
+@dataclass
+class ReturnResultPro(Generic[U, V]):
+    """
+    返回结果
+    """
+    status: bool = True
+    message: str = ""
+    data: U = None
+    addon: V = None
+
+
+# 2. 定义泛型类型的方式2：直接在方法名称后面通过方括号[S, K]指定泛型参数
+# 此种方式使用的泛型类型可直接使用，不需要使用类似U = TypeVar("U")的方式提前声明
+@dataclass
+class ReturnResultExt[S, K]:
+    """
+    返回结果
+    """
+    status: bool = True
+    message: str = ""
+    data: S = None
+    extra: K = None
+
+
 if __name__ == '__main__':
     print("Hello, world!")
 
-    # 2. 使用泛型类型
+    # 使用泛型类型
     result_str = ReturnResult[str](status=True, message="成功", data="Hello World")
     result_int = ReturnResult[int](status=True, message="成功", data=123)
     result_dict = ReturnResult[dict](status=True, message="成功", data={"name": "张三", "age": 30})
@@ -50,3 +75,10 @@ if __name__ == '__main__':
     print(result_int.data)
     print(result_dict.data)
     print(result_dict.data["name"])
+
+    print("──分割线───────────────────────────────────")
+    # 使用泛型类型
+    result_ext_a = ReturnResultExt[str, list](status=True, message="成功", data="Hello World", extra=[1, 2, 3])
+    print(result_ext_a.extra)
+    result_ext_b = ReturnResultExt[str, str](status=True, message="成功", data="Hello World", extra="山东解大劦")
+    print(result_ext_b.extra)
